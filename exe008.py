759e82fb7f71937b87b7e3f84e58d059686319d9
marcos <marcosfoxy655>
2025-12-26 16:31:08 -0300

    exercicio 008 conversão de medidas

---
+++ exe008.py
@@ -0,0 +1,14 @@
+metros=float(input('digite um numero em metros'))
+kilometros=metros*10*10*10
+hectometros=kilometros/10
+decametros=hectometros/10
+decimetro=decametros/10/10
+centimetros=decimetro/10
+milimetros=centimetros/10
+print(f'{kilometros}km')
+print(f'{hectometros}hm')
+print(f'{decametros}dam')
+print(f'{metros}m')
+print(f'{decimetro}dm')
+print(f'{centimetros}cm')
+print(f'{milimetros}mm')
