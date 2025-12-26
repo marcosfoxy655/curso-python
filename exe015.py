d100e48ae449fbd16c2faaacc69e6c60537cab99
marcos <marcosfoxy655>
2025-12-26 20:24:47 -0300

    exercicio 015 aluguel de carro

---
+++ exe015.py
@@ -0,0 +1,3 @@
+dias=int(input('quantos dias vocé alugol o carro? '))
+quilometros=float(input('quantos quilometros vocé andou com o carro? '))
+print(f'vocé tera que pagar ao todo {(dias*60)+(quilometros*0.15)} pois os dias custam R$60 e vocé usou por {dias} e isso da {dias*60} e a cada kilometro e R$0,15 e vocé andou {quilometros}Km então deu {quilometros*0.15}')
