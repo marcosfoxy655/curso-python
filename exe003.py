01d593dbfcf457b2830d00089b0eac9a6acd28c1
marcos <marcosfoxy655>
2025-12-26 16:27:25 -0300

    exercicio 003 vamos somar

---
+++ exe003.py
@@ -0,0 +1,4 @@
+numero1=int(input('digite um numero: '))
+numero2=int(input('digite outro numero: '))
+calculo=numero1 + numero2
+print(f'o numero {numero1} mais o numero {numero2} e igual ao numero {calculo}')
