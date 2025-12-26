895211eed62f071ea130beb32cd200a3800b7d24
marcos <marcosfoxy655>
2025-12-26 16:30:09 -0300

    exercicio 006 dobro triplo raiz quadrada

---
+++ exe006.py
@@ -0,0 +1,8 @@
+numero=int(input('digite um numero: '))
+dobro=numero*2
+triplo=numero*3
+raiz_quadrada=numero**(1/2)
+
+print(f'o dobro de {numero} é {dobro}')
+print(f'o triplo de {numero} é {triplo}')
+print(f'a raiz quadrada de {numero} é {float(raiz_quadrada)}')
