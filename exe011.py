c7f22d63f8aaf4ed9b330f55fb63e9602eb4a08d
marcos <marcosfoxy655>
2025-12-26 17:26:41 -0300

    exercicio 011 pintando uma parede

---
+++ exe011.py
@@ -0,0 +1,5 @@
+largura=float(input('digite a largura da sua parede? '))
+altura=float(input('digite a altura da sua parede? '))
+area=largura*altura
+litros=area/2
+print(f'a sua area e de {area}M² .\n e a quantidade de litros para pintar essa parede e de {litros}L')
