7753737a79aca2207354e49f8c1542cf3e3d372c
marcos <marcosfoxy655>
2025-12-27 18:50:45 -0300

    exercio 026 posição da letra A e quantas vezes apareceu #8

---
+++ exe026.py
@@ -0,0 +1,5 @@
+palavra = str(input('escreva uma palavra: ')).upper()
+
+print(f'a letra A aparece {palavra.count('A')} de vezes')
+print(f'a primeira letra A apareceu na posição {palavra.find('A')+1}')
+print(f'a ultima letra A apareceu na posição {palavra.rfind('A')+1}')
