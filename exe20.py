738d04c70d01d7eb0997c9cc87776befd39ecbb1
marcos <marcosfoxy655>
2025-12-27 09:45:53 -0300

    exercicio 020 duvida

---
+++ exe20.py
@@ -0,0 +1,8 @@
+import random
+primeiro=(input('qual o nome do primeiro aluno? '))
+segundo=(input('qual o nome do segundo aluno? '))
+terceiro=(input('qual o nome do terceiro aluno? '))
+quarto=(input('qual o nome do quarto aluno? '))
+lista=(primeiro, segundo, terceiro, quarto)
+random.shuffle(lista)
+print('a ordem da chamada sera')
