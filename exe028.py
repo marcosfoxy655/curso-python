985dad67d93afee1850c8768bddd72f02b42c857
marcos <marcosfoxy655>
2025-12-27 20:29:04 -0300

    exercicio 028 adinvinhando #11 Open

---
+++ exe028.py
@@ -0,0 +1,10 @@
+print('==='*15)
+print('estou pensando em um numero entre 0 e 5')
+print('==='*15)
+print('')
+numero=int(input('qual numero você acha que eu estou pensando? '))
+if numero == 4 :
+    print('parabem o numero era realmente 4')
+else:
+    print(f'você errou o numero não era {numero} o numero era 4')
+print('--fim--')
