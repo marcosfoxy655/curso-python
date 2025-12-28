085353374e000eee7fe089b706865ad2ba658e75
marcos <marcosfoxy655>
2025-12-28 09:49:37 -0300

    exercio 029 correção de bug #15

--- exe029.py
+++ exe029.py
@@ -1,5 +1,5 @@
 velocidade=float(input('qual a velocidade atual do carro? '))
 if velocidade > 80.0:
     multa= (velocidade-80.0)*7.00
-    print(f'MULTADO você utrapassou a velocidade maxima de 80\n a multa e de {multa:.}')
+    print(f'MULTADO você utrapassou a velocidade maxima de 80\n a multa e de {multa:.2f}')
 print('dirija com segurança thau')
