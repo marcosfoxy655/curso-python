d9ccfd47f79cc61acb3f3622f95276c855ac0a44
marcos <marcosfoxy655>
2025-12-28 09:06:53 -0300

    exercicio 008 corrigindo bug #14

--- exe008.py
+++ exe008.py
@@ -1,10 +1,10 @@
 metros=float(input('digite um numero em metros'))
-kilometros=metros*10*10*10
-hectometros=kilometros/10
-decametros=hectometros/10
-decimetro=decametros/10/10
-centimetros=decimetro/10
-milimetros=centimetros/10
+kilometros=metros/10/10/10
+hectometros=kilometros*10
+decametros=hectometros*10
+decimetro=decametros*10*10
+centimetros=decimetro*10
+milimetros=centimetros*10
 print(f'{kilometros}km')
 print(f'{hectometros}hm')
 print(f'{decametros}dam')
