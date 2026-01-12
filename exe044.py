10b40d05cd45235e4fa622d18a0ae25953ce7c6d
marcos <marcosfoxy655>
2026-01-12 10:58:06 -0300

    exercicio 044 gerenciado de pagamento #29

---
+++ exe044.py
@@ -0,0 +1,22 @@
+print('{:=^50}'.format(' LOJAS RAPOSAS '))
+preço=float(input('qual o preço da compra: '))
+print(''' FORMAS DE PAGAMENTO
+[1] avista dinheiro/cheque
+[2] avista cartão
+[3] ate 2x no cartão
+[4] 3x ou mais no cartão
+''')
+opção=int(input('qual das formas de pagamento você? '))
+if opção == 1:
+    preço_final=preço - (preço / 100 * 10)
+    print(f'você pagou {preço_final}')
+elif opção == 2:
+    preço_final=preço - (preço / 100 * 5)
+    print(f'você pagou {preço_final}')
+elif opção == 3:
+    print(f'você pagou {preço}')
+elif opção == 4:
+    preço_final=preço + (preço / 100 * 20)
+    print(f'você pagou {preço_final}')
+else:
+    print('ERRO digite un dos numeros acima')
