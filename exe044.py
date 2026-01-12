84a10d4a7fb14ce9cf19d661e51903eef0faf11c
marcos <marcosfoxy655>
2026-01-12 11:30:49 -0300

    exercicio 044 adiciando quantidades de parcelas #30

--- exe044.py
+++ exe044.py
@@ -3,8 +3,8 @@ preço=float(input('qual o preço da compra: '))
 print(''' FORMAS DE PAGAMENTO
 [1] avista dinheiro/cheque
 [2] avista cartão
-[3] ate 2x no cartão
-[4] 3x ou mais no cartão
+[3] ate 2x no cartão sem juros
+[4] 3x ou mais no cartão com juros
 ''')
 opção=int(input('qual das formas de pagamento você? '))
 if opção == 1:
@@ -14,9 +14,11 @@ elif opção == 2:
     preço_final=preço - (preço / 100 * 5)
     print(f'você pagou {preço_final}')
 elif opção == 3:
-    print(f'você pagou {preço}')
+    print(f'você pagou {preço_final}')
 elif opção == 4:
     preço_final=preço + (preço / 100 * 20)
+    parcelas=int(input('quantas parcelas'))
+    print(f'você parcelou em {parcelas}x')
     print(f'você pagou {preço_final}')
 else:
     print('ERRO digite un dos numeros acima')
